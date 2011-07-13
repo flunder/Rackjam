d55dcb6dfd52971dc41cc67f8704ce8213ab27d4
flunder <larsf2005@gmail.com>
2011-07-13 16:35:17 +0100

    ebay

--- Gemfile
+++ Gemfile
@@ -1,14 +1,16 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
+gem "jquery-rails", "~> 1.0.12"
 gem 'sqlite3'
 gem 'mysql'
+gem "will_paginate"
+gem "permalink_fu"
 gem 'scrapi'
 gem 'feedzirra'
 gem "paperclip", "~> 2.3.11"
 gem 'acts-as-taggable-on'
-gem "jquery-rails", "~> 1.0.12"
 gem 'oauth'
 gem "twitter", "~> 1.6.0"
-gem "will_paginate"
-gem 'permalink_fu'
+gem "ebayer", "~> 1.2.0"
+ gem 'httparty'
--- app/models/item.rb
+++ app/models/item.rb
@@ -36,7 +36,20 @@ class Item < ActiveRecord::Base
   
   def self.get() 
       tresh = 10; 
-      self.update_via_feed('gumtree', 'http://www.gumtree.com/cgi-bin/list_postings.pl?feed=rss&posting_cat=4709&search_terms=instruments', tresh)
+      # self.update_via_feed('gumtree', 'http://www.gumtree.com/cgi-bin/list_postings.pl?feed=rss&posting_cat=4709&search_terms=instruments', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Sequencers-Grooveboxes-/58721/i.html?LH_PrefLoc=0&LH_Price=30..%40c&rt=nc&_catref=1&_dlg=1&_dmpt=UK_Musical_Instruments_Sequencers_Grooveboxes_MJ&_ds=1&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Computer-Recording-Software-/23784/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_MusicalInstruments_ComputerRecording_Software_SM&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Mixers-Mixer-Accessories-/23785/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Mixers&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Monitors-/23786/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_ConElec_SpeakersPASystems_RL&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Midi-Controllers-/14987/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=Midi_Controllers&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Racks-Cases-/23789/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Musical_Instruments_Rack_Cases_MJ&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Other-Pro-Audio-Equipment-/3278/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Outboards-Effects-/23791/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Musical_Instruments_Outboards_Effects_MJ&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Midi-Audio-Interfaces-/123445/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=Midi_Controllers&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Drum-Machines-/38069/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Drum_Machines_Grooveboxes&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Samplers-Sampler-Accessories-/38070/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Musical_Instruments_Pro_Audio_Samplers_Accessories_CV&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Synthesisers-Sound-Modules-/38071/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Musical_Instruments_Pro_Audio_Synthesisers_CV&_mPrRngCbx=1&_rss=1', tresh)
+      self.update_via_feed('ebay', 'http://musical-instruments.shop.ebay.co.uk/Recorders-Rewriters-/15199/i.html?LH_PrefLoc=1&LH_Price=50..%40c&rt=nc&_catref=1&_dmpt=UK_Recorders_Rewriters&_mPrRngCbx=1&_rss=1', tresh)                        
   end
 
   def self.update_via_feed(sourceName, url, tresh)
@@ -45,8 +58,9 @@ class Item < ActiveRecord::Base
       feed = Feedzirra::Feed.fetch_and_parse(url)
   
       feed.entries.each_with_index do |entry,index|
-        
+              
             item = feedSpecificFields(sourceName).scrape(open(entry.url).read)
+            
             if !item[0].title  # No headline sounds like trouble, lets skip it
                 puts "Missing headline ~ wtf!"
             else
@@ -59,6 +73,7 @@ class Item < ActiveRecord::Base
                 else 
                   imageSrc = item[0].imageSrc # has image
                 end
+                
                 title = item[0].title.strip      
                 item[0].desc ||= " " 
                 desc  = item[0].desc.strip
@@ -71,9 +86,26 @@ class Item < ActiveRecord::Base
                     title = title[0..title.index('&amp;')-1] 
                   end
                 end
+                
+                # -- EBAY  ------------------------------------------- 
+                  if sourceName == 'ebay' 
+
+                    # IGNORE SELLERS
+                    # if item[0].seller == 'fudgescyclestore' || item[0].seller == 'dragonssoup' || item[0].seller == 'tritoncycles' 
+                    #   skip = 'yes';
+                    # end 
+
+                    price = item[0].priceAuction
+
+                    # Cut the cents off
+                    if (price.rindex('.'))
+                      price = price[1..price.rindex('.')-1] # 1 for the leading q-mark
+                    end
+                  end                
                 #END SPECIFOLICS ----------- **
                 
                 price = self.cleanPrice(price)              
+             
                 
                 if skip == 'no' 
 
@@ -151,14 +183,13 @@ class Item < ActiveRecord::Base
         return scraper = Scraper.define do
           array :items
           process "body", :items => Scraper.define {
-            process "h1", :headline => :text
-            process "h1", :blurb => :text #tricky
+            process "h1", :title => :text
+            process "h1", :desc => :text #tricky
             # process "td.ipics-cell center img", :imageSrc => "@src"
             process "div.vi-ipic1 center img", :imageSrc => "@src"
             process "td.vi-is1-tbll>span>span", :priceAuction => :text
             process "span.mbg-nw", :seller => :text
-
-            result :headline, :imageSrc, :priceAuction, :blurb, :seller
+            result :title, :imageSrc, :desc, :priceAuction, :seller
           }
           result :items
         end
@@ -169,11 +200,11 @@ class Item < ActiveRecord::Base
            array :items
            process ".posting", :items => Scraper.define {
              process "h2", :headline => :text
-             process "#userbody", :blurb => :text
+             process "#userbody", :desc => :text
              process "table img", :imageSrc => "@src"
              process "h2", :price => :text
              process "#posting-map img", :location => "@src"
-             result :headline, :imageSrc, :blurb, :price, :location
+             result :title, :imageSrc, :desc, :price, :location
            }
            result :items
          end
@@ -185,10 +216,10 @@ class Item < ActiveRecord::Base
            array :items
            process ".layout100", :items => Scraper.define {
              process "h1", :headline => :text
-             process "tr>td", :blurb => :text
+             process "tr>td", :desc => :text
              process ".lightbox", :imageSrc => "@href"
              process "table tr:nth-child(2) td:nth-child(3)", :price => :text
-             result :headline, :imageSrc, :blurb, :price
+             result :headline, :imageSrc, :desc, :price
            }
            result :items
          end
