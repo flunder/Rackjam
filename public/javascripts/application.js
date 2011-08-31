c956734959131408cb54d9b5fad49f1edaaf7340
flunder <larsf2005@gmail.com>
2011-08-31 12:48:58 +0100

    sign in looks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,3 +16,29 @@ $(document).ready(function(){
 		$.cookie( "rackjamnotice", "closed", { expires: 2 } ); 	
 	})
 });
+
+$(document).ready(function() {
+	    $('.form span.defaultValue').each(function() {
+    
+	        var defaultValue = $(this).text();
+	        var inputField = $(this).siblings('input');
+        
+	        inputField.attr("default",defaultValue) /* add the default attribute ~ <input type="" default="dd/mm/yyy" */
+	        if (inputField.val() == '' || inputField.val() == defaultValue) {
+	            inputField.val(defaultValue).addClass('defaultValueFields').addClass('initValue')
+	        } 
+	    });
+     
+	    /* Handle a click on a defaultValue field */
+	    $(".defaultValueFields").live('focus',function() {   
+	        $(this).removeClass('initValue')
+    
+	        if( $(this).val() == $(this).attr("default") ) {
+	            $(this).val('');
+	        }
+	    }).blur(function() {
+	        if( !$(this).val().length ) {
+	            $(this).val($(this).attr("default")).addClass('initValue');
+	        }
+	    });  
+});	
